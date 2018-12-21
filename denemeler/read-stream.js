dc5229df287a2a3888693201028b2e7b88702270
Tolgahan DEMİRBAŞ <todehan@gmail.com>
2018-12-21 14:13:16 +0300

    Basit bir node stream yapısı

---
+++ denemeler/read-stream.js
@@ -0,0 +1,21 @@
+const fs = require ('fs');
+const file = 'video.mp4';
+
+const readStream = fs.createReadStream(file);
+
+let progress = 0;
+
+fs.stat(file, (err, data) => {
+    const total = data.size;
+
+    readStream.on('data', (chunk) =>{
+        progress += chunk.length;
+        console.log(Math.round((100 * progress) / total) + '%');
+    });
+
+    readStream.on('end', ()=> {
+        console.log('Veri okuma işlemi bitti');
+    })
+
+
+});
